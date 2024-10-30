8bdc4f39f982fdac52e37d7fc4b0536be987ea2f
Timothy Holmes <holmestimo11@gmail.com>
2024-10-30 17:25:16 -0500

    completed cmx-88

--- src/core/migrations/0005_create_product_preferences_tables.py
+++ src/core/migrations/0005_create_product_preferences_tables.py
@@ -9,7 +9,13 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql="""
                 create table product_characteristics(
-
+                product_id bigint primary key References vendor_product(product_id) on delete cascade,
+                cb_taste_strength char(1) CHECK (cb_taste_strength ~ '[0-9]'), 
+                cb_decaf char(1) CHECK (cb_decaf in ('Y', 'N')), 
+                cb_flavored char(1) CHECK (cb_flavored in ('Y', 'N')),
+                cb_single_origin char(1) CHECK (cb_single_origin in ('Y', 'N')),
+                cb_regions varchar(100),
+                cb_keywords varchar(100)
                 );
 
                 create table customer_preferences(
