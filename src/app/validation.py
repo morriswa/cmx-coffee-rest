bddd6b6d13300bdea69f638fc799e58694a8ed3c
morriswa <william@morriswa.org>
2024-10-31 11:03:52 -0500

    move is_blank to validation package

--- src/app/validation.py
+++ src/app/validation.py
@@ -1,11 +1,29 @@
 from abc import ABC
 from copy import deepcopy
+from typing import Optional
+
+
+def is_blank(data: Optional[str]) -> bool:
+    if data is None:
+        return True
+
+    data_str = str(data)
+    if len(data_str) == 0:
+        return True
+
+    if len(data_str.strip()) == 0:
+        return True
+
+    return False
+
 
 
 class ValidatedDataModel(ABC):
     def validate(self) -> None:
         raise NotImplementedError('validate method has not been implemented')
+
     def copy(self):
         return deepcopy(self)
+
     def json(self) -> dict:
         return vars(self)
