fcd41890678c9809680f1b8164197a7759e73f61
Timothy Holmes <holmestimo11@gmail.com>
2024-11-24 12:02:04 -0600

    added more to cmx-142

--- src/vendor/tests/daos.py
+++ src/vendor/tests/daos.py
@@ -5,8 +5,10 @@ from django.test import TestCase
 from app.authentication import User
 from app import connections
 from app.exceptions import BadRequestException
-
+from vendor.models import VendorApplicationResponse
+from admin.models import AdminVendorInfo
 import admin.daos as admin_dao
+
 
 
 class ApplicationForVendorDAOTest(TestCase):
@@ -36,3 +38,49 @@ class ApplicationForVendorDAOTest(TestCase):
             """, {
                 'applicant_id': applicant_user_id
             })
+
+    def test_apply_for_vendor(self):
+        """ ensure dao correctly applies for vendor """
+        
+        # setup
+        applicant_user_id = self.__setup_apply_for_vendor()
+        vendor_application = VendorApplicationResponse(
+            business_name='test application 1',
+            business_email='test email',
+            phone='1231231234',
+            address_one='1234 Address Line One',
+            address_two=None,
+            city='City',
+            zip='55555',
+            territory_id='TEST_CASE'
+        )
+
+        #execute 
+        VendorApplicationResponse = admin_dao.apply_for_vendor(vendor_application)
+
+        #asset 
+        self.assertTrue(
+            isinstance(VendorApplicationResponse, VendorApplicationResponse),
+            'correct body format (json list)'
+        )
+
+        self.assertEqual(
+            VendorApplicationResponse.business_name,
+            'test application 1',
+            'correct value retrieved from db'
+        )
+
+class ProcessPendingVendorApplicationDAOTests(TestCase):
+    def __setup_process_pending_vendor_application(self):
+        """ database setup for approve_pending_vendor_application test """
+
+        gen_application_id = None
+        applicant_user_id = uuid.uuid4();
+        admin_user_id = uuid.uuid4();
+        with connections.cursor() as cur:
+            cur.execute("""
+                insert into auth_integration
+                    (user_id, email)
+                values
+                    (%(applicant_id)s, """)
+            
