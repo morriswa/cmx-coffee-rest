9c60c0a0c51b257d6193a4ee240ff2bf5fac98d1
morriswa <william@morriswa.org>
2024-12-03 14:40:15 -0600

    2

--- test.py
+++ test.py
@@ -18,22 +18,12 @@ def test():
     """Run test tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.app.test')
 
-    test_modules = []
     flags = sys.argv[1:]
 
     if "test" in flags:
         flags.remove("test")
 
-    if "--all" in flags:
-        test_modules += settings.APP_MODULES
-        flags.remove("--all")
-    elif "--with" in flags:
-        test_modules += flags[flags.index("--with") + 1:]
-        flags = flags[0:flags.index("--with")]
-    else:
-        test_modules += settings.APP_MODULES
-
-    command = ["./manage.py", "test", *flags, *test_modules]
+    command = ["test.py", "test", *flags, *settings.APP_MODULES]
     print(f'executing tests from command line with: {command}')
 
     from django.core.management import execute_from_command_line
