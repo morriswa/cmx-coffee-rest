802c055edb3d02bc9a9d1621af8bc03b563cc27c
morriswa <william@morriswa.org>
2024-12-03 14:12:11 -0600

    fix vscode test integration

--- test.py
+++ test.py
@@ -17,15 +17,28 @@ from django.conf import settings
 def test():
     """Run test tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.app.test')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line([sys.argv[0], "test", *sys.argv[1:], *settings.APP_MODULES])
+
+    test_modules = []
+    flags = sys.argv[1:]
+
+    if "test" in flags:
+        flags.remove("test")
+
+    if "--all" in flags:
+        test_modules += settings.APP_MODULES
+        flags.remove("--all")
+    elif "--with" in flags:
+        test_modules += flags[flags.index("--with") + 1:]
+        flags = flags[0:flags.index("--with")]
+    else:
+        test_modules += settings.APP_MODULES
+
+    command = ["./manage.py", "test", *flags, *test_modules]
+    print(f'executing tests from command line with: {command}')
+
+    from django.core.management import execute_from_command_line
+    assert execute_from_command_line != None
+    execute_from_command_line(command)
 
 
 if __name__ == '__main__':
